ec135d4b104cafc68c7e09bcf1c79f062135a99d
mustafalmazz <mustafagfb19077091@gmail.com>
2025-12-26 19:38:20 +0300

    model updated

--- TechStoreApi/Models/Product.cs
+++ TechStoreApi/Models/Product.cs
@@ -10,6 +10,7 @@ namespace TechStoreApi.Models
         public decimal? Price { get; set; }
         public int? Stock { get; set; }
         public int? CategoryId { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
         public Category? Category { get; set; }
     }
 }
